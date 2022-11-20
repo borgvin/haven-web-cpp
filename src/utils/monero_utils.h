a9d305a190c2afdd978c0417f9a1be28e160bf06
MartyHav <51510876+MartyHav@users.noreply.github.com>
2022-11-20 19:17:24 +0100

    add json conversion

--- src/utils/monero_utils.h
+++ src/utils/monero_utils.h
@@ -110,6 +110,8 @@ namespace monero_utils
   rapidjson::Value to_rapidjson_val(rapidjson::Document::AllocatorType& allocator, const std::vector<uint32_t>& nums);
   rapidjson::Value to_rapidjson_val(rapidjson::Document::AllocatorType& allocator, const std::vector<uint64_t>& nums);
   rapidjson::Value to_rapidjson_val(rapidjson::Document::AllocatorType& allocator, const std::map<std::string, uint64_t>& map);
+  rapidjson::Value to_rapidjson_val(rapidjson::Document::AllocatorType& allocator, const std::map<std::string, std::string>& map);
+
 
   // ------------------------ PROPERTY TREES ---------------------------
 
